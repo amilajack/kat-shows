5d87e3f3799ddda37d997246c49569c943522c38
Amila Welihinda <amilajack@gmail.com>
2020-04-10 23:06:03 -0700

    more config boilerplate changes

--- src/Torrent.js
+++ src/Torrent.js
@@ -4,34 +4,29 @@
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
-export const BASE_URL = 'https://kat.cr';
+export const BASE_URL = 'https://kickass.ws';
 
 export function formatShowNameToUrl(showName) {
-  return (`/${showName}`).toLowerCase().replace(/ /g, '-');
+  return `/${showName}`.toLowerCase().replace(/ /g, '-');
 }
 
 export async function parseShows() {
-  try {
-    const showsHtml = await fetch(`${BASE_URL}/tv/show`, {
-      mode: 'no-cors'
+  const showsHtml = await fetch(`${BASE_URL}/tv/shows`, {
+    mode: 'no-cors',
+  }).then((response) => response.text());
+
+  const $ = cheerio.load(showsHtml);
+
+  const items = $('.textcontent a.plain')
+    .filter(function filterEmptyUrls() {
+      return $(this).attr('href') !== '#' && !!$(this).attr('href');
     })
-      .then((response) => response.text());
+    .map(function parseShowsHtml() {
+      return $(this).attr('href');
+    })
+    .get();
 
-    const $ = cheerio.load(showsHtml);
-
-    const items = $('.textcontent a.plain')
-      .filter(function filterEmptyUrls() {
-        return $(this).attr('href') !== '#' && !!$(this).attr('href');
-      })
-      .map(function parseShowsHtml() {
-        return $(this).attr('href');
-      })
-      .get();
-
-    return items;
-  } catch (err) {
-    console.log(err);
-  }
+  return items;
 }
 
 export async function findShowUrl(showName) {
@@ -40,8 +35,8 @@ export async function findShowUrl(showName) {
   const parsedShows = await parseShows();
   const formattedShowUrl = formatShowNameToUrl(showName);
 
-  const showUrl = parsedShows.find(
-    (showUrls) => showUrls.includes(formattedShowUrl)
+  const showUrl = parsedShows.find((showUrls) =>
+    showUrls.includes(formattedShowUrl)
   );
 
   if (!showUrl) {
@@ -54,9 +49,8 @@ export async function findShowUrl(showName) {
 export async function findEpisodeId(showName, season, episode) {
   const showUrl = await findShowUrl(showName);
   const episodeTorrentsHtml = await fetch(showUrl, {
-    mode: 'no-cors'
-  })
-    .then((response) => response.text());
+    mode: 'no-cors',
+  }).then((response) => response.text());
 
   const $ = cheerio.load(episodeTorrentsHtml);
 
@@ -70,11 +64,11 @@ export async function findEpisodeId(showName, season, episode) {
   const indexes = [];
   const ids = [];
 
-  for (let i = 0; i < episodeUrl.length; i++) {
+  for (let i = 0; i < episodeUrl.length; i += 1) {
     if (episodeUrl[i] === "'") indexes.push(i);
   }
 
-  for (let i = indexes[0] + 1; i < indexes[1]; i++) {
+  for (let i = indexes[0] + 1; i < indexes[1]; i += 1) {
     ids.push(episodeUrl[i]);
   }
 
@@ -84,19 +78,22 @@ export async function findEpisodeId(showName, season, episode) {
 export default async function parseTorrent(showName, season, episode) {
   const episodeId = await findEpisodeId(showName, season, episode);
 
-  const torrentsHtml = await fetch(`${BASE_URL}/media/getepisode/${episodeId}`)
-    .then((res) => res.text());
+  const torrentsHtml = await fetch(
+    `${BASE_URL}/media/getepisode/${episodeId}`
+  ).then((res) => res.text());
 
   const $ = cheerio.load(torrentsHtml);
 
-  const torrents = $("table tr:not('.firstr')").map(function formatTorrents() {
-    return {
-      magnet: $(this).find('[title="Torrent magnet link"]').attr('href'),
-      metadata: $(this).find('[title="Torrent magnet link"]').attr('href'),
-      seeders: parseInt($(this).find('.green.center').text(), 10),
-      leechers: parseInt($(this).find('.red.lasttd.center').text(), 10)
-    };
-  }).get();
+  const torrents = $("table tr:not('.firstr')")
+    .map(function formatTorrents() {
+      return {
+        magnet: $(this).find('[title="Torrent magnet link"]').attr('href'),
+        metadata: $(this).find('[title="Torrent magnet link"]').attr('href'),
+        seeders: parseInt($(this).find('.green.center').text(), 10),
+        leechers: parseInt($(this).find('.red.lasttd.center').text(), 10),
+      };
+    })
+    .get();
 
   return torrents;
 }
--- test/torrent.spec.js
+++ test/torrent.spec.js
@@ -1,177 +1,108 @@
-/**
- * Test all high level methods
- *
- * @todo: reduced the number of api calls by querying once and running multiple
- *        tests against that query. ideally, this would be done in a 'before'
- *        function
- */
-
-/*
- eslint no-unused-expressions: 0, new-cap: 0 no-console: 0, func-names: 0, no-use-before-define: 0
-*/
-import { expect } from 'chai';
 import Torrent, {
   findShowUrl,
   findEpisodeId,
   parseShows,
-  formatShowNameToUrl
+  formatShowNameToUrl,
+  BASE_URL,
 } from '../src/Torrent';
 
+function assertTorrentsFormat(torrents) {
+  expect(torrents).toBeInstanceOf(Array);
+  const torrent = torrents[0];
+  // expect(torrent).to.be.an('object');
+  expect(torrent).toHaveProperty('magnet');
+  expect(torrent.magnet).toContain('magnet:?');
+  expect(torrent).toHaveProperty('seeders');
+  expect(torrent).toHaveProperty('leechers');
+}
 
-describe('api', () => {
+describe('API', () => {
   describe('Parser', () => {
-    it('should return promise', async done => {
-      try {
-        expect(findShowUrl('game of thrones')).to.be.a('promise');
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should return promise', async () => {
+      expect(findShowUrl('game of thrones')).toBeInstanceOf(Promise);
     });
 
-    it('should correctly format show names to url slugs', done => {
-      try {
-        expect(formatShowNameToUrl('Game of Thrones')).to.equal('/game-of-thrones');
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should correctly format show names to url slugs', () => {
+      expect(formatShowNameToUrl('Game of Thrones')).toEqual(
+        '/game-of-thrones'
+      );
     });
 
-    it('should parse shows page and return array of show urls', async done => {
-      try {
-        const showUrls = await parseShows();
-        expect(showUrls).to.be.an('array');
-        const formattedShowUrl = formatShowNameToUrl('game of thrones');
+    it('should parse shows page and return array of show urls', async () => {
+      const showUrls = await parseShows();
+      // expect(showUrls).to.be.an('array');
+      const formattedShowUrl = formatShowNameToUrl('game of thrones');
 
-        for (const url of showUrls) {
-          expect(url).to.be.a('string');
-        }
+      // for (const url of showUrls) {
+      //   expect(url).to.be.a('string');
+      // }
 
-        const showUrl = showUrls.find(
-          item => item.includes(formattedShowUrl)
-        );
+      const showUrl = showUrls.find((item) => item.includes(formattedShowUrl));
 
-        expect(showUrl).to.equal('/game-of-thrones-tv11489/');
-        done();
-      } catch (err) {
-        done(err);
-      }
+      expect(showUrl).toEqual('/game-of-thrones-tv11489/');
     });
 
-    it('should find show url and return array of links', async done => {
-      try {
-        const showName = 'game of thrones';
-        const parsedShows = await parseShows();
-        const showUrl = parsedShows.find(
-          item => item.includes(formatShowNameToUrl(showName))
-        );
+    it('should find show url and return array of links', async () => {
+      const showName = 'game of thrones';
+      const parsedShows = await parseShows();
+      const showUrl = parsedShows.find((item) =>
+        item.includes(formatShowNameToUrl(showName))
+      );
 
-        expect(parsedShows).to.be.an('array');
+      expect(parsedShows).toBeInstanceOf(Array);
 
-        for (const eachShowUrl of parsedShows) {
-          expect(eachShowUrl).to.be.an('string');
-        }
+      // for (const eachShowUrl of parsedShows) {
+      //   expect(eachShowUrl).to.be.an('string');
+      // }
 
-        expect(showUrl).to.be.an('string');
-        done();
-      } catch (err) {
-        done(err);
-      }
+      expect(showUrl).toBeInstanceOf(String);
     });
 
-    it('should return a url: 1', async done => {
-      try {
-        expect(await findShowUrl('game of thrones'))
-          .to
-          .equal('https://kat.cr/game-of-thrones-tv11489/');
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should return a url: 1', async () => {
+      expect(await findShowUrl('game of thrones')).toEqual(
+        `${BASE_URL}/game-of-thrones-tv11489/`
+      );
     });
 
-    it('should return a url: 2', async done => {
-      try {
-        expect(await findShowUrl('prison break'))
-          .to.equal('https://kat.cr/prison-break-tv2769/');
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should return a url: 2', async () => {
+      expect(await findShowUrl('prison break')).toEqual(
+        `${BASE_URL}/prison-break-tv2769/`
+      );
     });
 
-    it('should find shows with capitalized params', async done => {
-      try {
-        expect(await findShowUrl('American Dad'))
-          .to.equal('https://kat.cr/american-dad%21-tv1260/');
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should find shows with capitalized params', async () => {
+      expect(await findShowUrl('American Dad')).toEqual(
+        `${BASE_URL}/american-dad%21-tv1260/`
+      );
     });
 
-    it('should return movies', async done => {
-      try {
-        expect(await findEpisodeId('Game of thrones', '02', '02'))
-          .to.equal('237994168');
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should return movies', async () => {
+      expect(await findEpisodeId('Game of thrones', '02', '02')).toEqual(
+        '237994168'
+      );
     });
 
-    it('should return movies with lowercase query', async done => {
-      try {
-        expect(await findEpisodeId('game of thrones', '02', '02'))
-          .to.equal('237994168');
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should return movies with lowercase query', async () => {
+      expect(await findEpisodeId('game of thrones', '02', '02')).toEqual(
+        '237994168'
+      );
     });
 
-    it('should return object with magnet, seeders, leechers: 1', async done => {
-      try {
-        const torrents = await Torrent('Game of thrones', '02', '02');
-        assertTorrentsFormat(torrents);
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should return object with magnet, seeders, leechers: 1', async () => {
+      const torrents = await Torrent('Game of thrones', '02', '02');
+      assertTorrentsFormat(torrents);
     });
 
-    it('should return object with magnet, seeders, lowercase query', async done => {
-      try {
-        const torrents = await Torrent('game of thrones', '02', '02');
-        assertTorrentsFormat(torrents);
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should return object with magnet, seeders, lowercase query', async () => {
+      const torrents = await Torrent('game of thrones', '02', '02');
+      assertTorrentsFormat(torrents);
     });
 
-    it('should return object with magnet, seeders, leechers: 2', async done => {
-      try {
-        const torrents = await Torrent('Game of thrones', '06', '01');
-        assertTorrentsFormat(torrents);
-        expect(torrents[0].seeders).to.be.greaterThan(200);
-        expect(torrents[2].seeders).to.be.greaterThan(200);
-        done();
-      } catch (err) {
-        done(err);
-      }
+    it('should return object with magnet, seeders, leechers: 2', async () => {
+      const torrents = await Torrent('Game of thrones', '06', '01');
+      assertTorrentsFormat(torrents);
+      expect(torrents[0].seeders).toBeGreaterThan(200);
+      expect(torrents[2].seeders).toBeGreaterThan(200);
     });
   });
 });
-
-function assertTorrentsFormat(torrents) {
-  expect(torrents).to.be.an('array');
-
-  const torrent = torrents[0];
-  expect(torrent).to.be.an('object');
-  expect(torrent).to.have.deep.property('magnet').that.is.a('string');
-  expect(torrent.magnet).to.include('magnet:?');
-  expect(torrent).to.have.deep.property('seeders').that.is.a('number');
-  expect(torrent).to.have.deep.property('leechers').that.is.a('number');
-}
